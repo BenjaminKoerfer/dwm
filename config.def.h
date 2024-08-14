b3a9156d68cd2f0264d3d2abea500c3adbcf3e78
Fritz-Lakritz <benjamin.koerfer@t-online.de>
2024-08-14 19:39:28 +0200

    hidden tags patch

--- config.def.h
+++ config.def.h
@@ -20,14 +20,19 @@ static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
 	[SchemeSel]  = { col_gray4, col_bar,  col_cyan  },
+	[SchemeStatus]  = { col_gray4, col_gray1,  "#000000"  }, // Statusbar right {text,background,not used but cannot be empty}
+	[SchemeTagsSel]  = { col_gray4, col_cyan,  "#000000"  }, // Tagbar left selected {text,background,not used but cannot be empty}
+	[SchemeTagsNorm]  = { col_gray3, col_gray1,  "#000000"  }, // Tagbar left unselected {text,background,not used but cannot be empty}
+	[SchemeInfoSel]  = { col_gray4, col_bar,  "#000000"  }, // infobar middle  selected {text,background,not used but cannot be empty}
+	[SchemeInfoNorm]  = { col_gray3, col_gray1,  "#000000"  }, // infobar middle  unselected {text,background,not used but cannot be empty}
 };
 static const unsigned int alphas[][3]      = {
     /*               fg      bg        border*/
-    [SchemeNorm] = { OPAQUE, baralpha, borderalpha },
+	[SchemeNorm] = { OPAQUE, baralpha, borderalpha },
 	[SchemeSel]  = { OPAQUE, baralpha, borderalpha },
 };
 /* tagging */
-static const char *tags[] = { "", "󰖟", "", "", "", "", "󰈫", "", "", "10", "11"};
+static const char *tags[] = { "", "", "", "", "󰖟", "", "󰈫", "", "", "10", "11"};
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -81,7 +86,8 @@ static const char *filebrowsercmd[]  = { "nemo", NULL };
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          SHCMD("dmenu_run -l 30 -fn 'FiraCode Nerd Font:size=20'") },
-	{ MODKEY,	       	        XK_x,	   spawn,          {.v = termcmd } },
+	{ MODKEY,	       	        XK_x,	   spawn,          SHCMD("st -e ~/work/scripts/tmuxsession") },
+	{ MODKEY|ShiftMask,    	        XK_x,	   spawn,          {.v = termcmd } },
 	{ MODKEY,		        XK_b,	   spawn,          {.v = browsercmd } },
 	{ MODKEY,		        XK_n,	   spawn,          {.v = filebrowsercmd } },
 	{ MODKEY|ShiftMask,	        XK_i,	   spawn,          SHCMD("~/work/scripts/bookmarks.sh")},
@@ -143,4 +149,3 @@ static const Button buttons[] = {
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
-
